7f495c38628b97bf2b75ec6964cf9f92088da6d2
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-10-15 10:04:59 -0700

    rough %c implementation

--- _printf.c
+++ _printf.c
@@ -1,6 +1,64 @@
 #include <stdio.h>
-int main(void)
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * check_char - checks if @arg is char
+ * @arg: argument to compare
+ * Return: returns 1 if arg is char, else 0.
+ */
+int check_char(va_list arg)
 {
-	printf("Philip\n");
-	return (0);
+	char c = va_arg(arg, int);
+
+	if ((c + '0') >= 0 && ((c + '0') <= '9'))
+		return (0);
+	return (c);
+}
+/**
+ * _printf - prints characters to the console
+ * @format: string format to print
+ * Return: returns number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int arg_count = 0;
+	int index = 0, c, index2 = 0;
+	char buff[45];
+
+	va_start(ap, format);
+
+	while (format[index])
+	{
+		if (format[index] == '%' && format[index + 1] == 'c')
+		{
+			c = check_char(ap);
+			if (c != 0)
+			{
+				buff[index2] = c;
+				index += 2;
+				index2++;
+				arg_count += 1;
+				continue;
+			}
+			else
+			{
+				write(2, "Error occured\n", 15);
+				exit(1);
+			}
+
+		}
+		buff[index2] = format[index];
+		arg_count += 1;
+
+		index++;
+		index2++;
+
+	}
+	buff[index2] = '\0';
+	write(1, buff, arg_count);
+	va_end(ap);
+	return (arg_count);
 }
---
+++ main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+int _printf(const char *format, ...);
+
+int main(void)
+{
+	int args;
+	char c = 'b';
+	char y = 'y';
+	char d = '1';
+
+	args = _printf("hello %co%c\nI love the way you role\nYou are my number: %c\n", c, y, d);
+	//printf("number of args printed: %d\n", args);
+	return (0);
+}
